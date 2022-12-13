cb304a279c91e8d7366d3a658b65f3f18ac1d2c0
ROGU3N1NJA <smeershoek2006@gmail.com>
2022-12-13 20:46:33 +0100

    fixed config issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  base: '/zenith-movie',
+  base: '/zenith-movie/',
   build: {
     target: 'esnext'
   },
